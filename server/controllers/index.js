e3541cbaba933e96098debf47305c2d885e21adc
FadiAlamassi <alamassifadi@gmail.com>
2019-09-06 23:49:33 +0300

    create handler to the login and signup hash the password using bcrypt and add the user to the database add cookie after the user login

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,17 +2,19 @@ const express = require('express');
 
 const error = require('./error');
 const city = require('./city');
-const signup = require('./signup');
-const login = require('./login');
+const { renderSignup, postSignup } = require('./signup');
+const { renderLogin, postLogin } = require('./login');
+const { auth } = require('./auth');
 
 const router = express.Router();
 
-router.get('/login', login.renderLogin);
-router.get('/signup', signup.renderSignup);
+router.get('/login', renderLogin);
+router.get('/signup', renderSignup);
+router.post('/login', postLogin)
+router.post('/signup', postSignup)
 router.get('/cities', city.renderCities);
 router.get('/all-cities', city.getAllCities);
 router.post('/add-city', city.add);
-
 router.use(error.client);
 router.use(error.server);
 
